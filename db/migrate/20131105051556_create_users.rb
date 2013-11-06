88112fbe985147d4db1f2c4d4cffff52308e1409
eirtracks <eirtracks0701@hotmail.co.jp>
2013-11-06 13:02:32 +0900

    ＤＢテーブル追加

--- db/migrate/20131105051556_create_users.rb
+++ db/migrate/20131105051556_create_users.rb
@@ -1,14 +1,18 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.integer :number
+      t.string :user_number
+      t.string :password_digest
       t.string :name
       t.string :country
       t.string :address
-      t.string :tel
-      t.string :work
-      t.integer :worktype
-      t.date :birth
+      t.string :phone
+      t.string :job
+      t.integer :job_type
+      t.date :birthday
       t.integer :gender
+      t.boolean :admin
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,60 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131030061404) do
+ActiveRecord::Schema.define(version: 20131105051556) do
+
+  create_table "after_graduation", id: false, force: true do |t|
+    t.string  "belong",     limit: nil
+    t.string  "position",   limit: nil
+    t.integer "work_type"
+    t.string  "other",      limit: nil
+    t.date    "alter_date"
+  end
+
+  create_table "graduate", id: false, force: true do |t|
+    t.boolean "change"
+    t.date    "finish"
+  end
+
+  create_table "meeting", id: false, force: true do |t|
+    t.date    "meeting_date"
+    t.string  "meeting_place", limit: nil
+    t.integer "event"
+  end
+
+  create_table "participant", id: false, force: true do |t|
+    t.integer "wish"
+    t.integer "wish_course"
+    t.string  "teacher",           limit: nil
+    t.boolean "entry_end"
+    t.date    "entry_participant"
+  end
+
+  create_table "student", id: false, force: true do |t|
+    t.integer "grade"
+    t.integer "department"
+    t.integer "research_subject"
+    t.integer "research_room"
+    t.string  "student_number",    limit: nil
+    t.string  "guarantor_name",    limit: nil
+    t.string  "guarantor_address", limit: nil
+    t.string  "guarantor_phone",   limit: nil
+    t.date    "entry"
+  end
 
   create_table "users", force: true do |t|
+    t.integer  "number"
+    t.string   "user_number"
+    t.string   "password_digest"
     t.string   "name"
     t.string   "country"
     t.string   "address"
-    t.string   "tel"
-    t.string   "work"
-    t.integer  "worktype"
-    t.date     "birth"
+    t.string   "phone"
+    t.string   "job"
+    t.integer  "job_type"
+    t.date     "birthday"
     t.integer  "gender"
+    t.boolean  "admin"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
