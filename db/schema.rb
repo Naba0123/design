72ace13b318b3aa1f391f169818dab5bbf4a3454
tatarhy <tat.j.kikyo@gmail.com>
2013-11-08 18:07:36 +0900

    ischangeをis_changeに変更

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131106084324) do
+ActiveRecord::Schema.define(version: 20131108065011) do
 
   create_table "users", force: true do |t|
     t.string   "user_id"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20131106084324) do
     t.integer  "wish"
     t.integer  "wish_course"
     t.string   "teacher"
-    t.boolean  "ischange"
+    t.boolean  "is_change"
     t.date     "finish_date"
     t.date     "meeting_date"
     t.string   "meeting_place"
