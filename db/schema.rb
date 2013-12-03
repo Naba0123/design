180381180cd30a06c02412b731a6c4faa26505cc
tatarhy <tat.j.kikyo@gmail.com>
2013-12-03 22:04:29 +0900

    マイグレーションファイルのリネーム

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131203091505) do
+ActiveRecord::Schema.define(version: 20131203111218) do
 
   create_table "after_graduations", force: true do |t|
     t.integer  "user_id"
