b077d1e838d5193e454385c4fba9a2f2803d45c1
Naba16 <mikumiku_dtm@yahoo.co.jp>
2013-12-19 20:11:13 +0900

    バグ（仕様）の説明とテストデータの編集（だがしかしテストはしていない）

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,34 +1,46 @@
 FactoryGirl.define do
   factory :arale, :class => User do
-    user_id  'arale'
+    account  'arale'
+    password 'hoge'
+    password_confirmation 'hoge'
     name     '則巻アラレ'
     country  'JP'
     address  'ゲンゴロウ島ペンギン村モモンガ1番地'
     phone    '00011112222'
     job      'ペンギン村村立中学園1年'
+    job_kind_id '1'
     birthday '1967-05-31'
     gender   1
+    authorized true
   end
 
   factory :akane, :class => User do
-    user_id  'akane'
+    account  'akane'
+    password 'hoge'
+    password_confirmation 'hoge'
     name     '木緑あかね'
     country  'US'
     address  'ゲンゴロウ島ペンギン村カモノハシ4番地'
     phone    '00011112222'
     job      'ペンギン村村立中学園1年'
+    job_kind_id '2'
     birthday '1967-12-21'
     gender   1
+    authorized true
   end
 
   factory :taro, :class => User do
-    user_id  'taro'
+    account  'taro'
+    password 'hoge'
+    password_confirmation 'hoge'
     name     '空豆タロウ'
     country  'JP'
     address  'ゲンゴロウ島ペンギン村アルマジロ9番地'
     phone    '00011112222'
     job      'ペンギン村村立中学園3年'
+    job_kind_id '3'
     birthday '1967-09-24'
     gender   0
+    authorized true
   end
 end
