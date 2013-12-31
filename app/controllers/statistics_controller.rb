acb5a2385ac0cf0a8d5c20d052c2b72702886b6a
makruk <tetsumasax@hotmail.com>
2013-12-31 17:08:40 +0900

    デザイン修正・バグがなければ完成

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,13 +1,17 @@
 ﻿class StatisticsController < ApplicationController
   def index
-    @queryNumber=params[:num].to_i
+    @nameArr=params[:name]
     @search=[]
     @queryString=params[:q]
     if @queryString != nil
+      @queryNumber=@queryString.size
       @queryNumber.times do |num|
         @search << User.search(@queryString[num])
       end
     else
+        @queryNumber=0
+        @nameArr=[]
+        @nameArr << "条件1"
         @search << User.search(@queryString)
     end
     @search << User.search(nil)
