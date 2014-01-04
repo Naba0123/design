35678900981750c492743f0e8260d06d5f1d01d5
tatarhy <tat.j.kikyo@gmail.com>
2014-01-04 22:50:10 +0900

    ログイン成功時のテストを追加

--- spec/features/authentication_pages_spec.rb
+++ spec/features/authentication_pages_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 feature 'ログイン' do
+  background do
+    FactoryGirl.create(:arale)
+  end
+
   scenario 'ユーザ認証失敗' do
     visit signin_path
     within('form#signin') do
@@ -10,5 +14,15 @@ feature 'ログイン' do
     end
     expect(page).to have_css('p.text-info', 'アカウント名またはパスワードが間違っています')
     expect(page).to have_css('form#signin')
+  end
+
+  scenario 'ユーザ認証成功' do
+    visit signin_path
+    within('form#signin') do
+      fill_in 'account', with: 'arale'
+      fill_in 'pass', with: 'hoge'
+      click_button 'Sign in'
+    end
+    expect(page).not_to have_css('form#signin')
   end
 end
