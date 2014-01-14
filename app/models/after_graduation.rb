5cb0373cf73c2eee5704b4d1f7b5cba2c07cc106
makruk <tetsumasax@hotmail.com>
2014-01-14 22:53:20 +0900

    バリデーション追加

--- app/models/after_graduation.rb
+++ app/models/after_graduation.rb
@@ -1,3 +1,6 @@
 class AfterGraduation < ActiveRecord::Base
 belongs_to :graduate
+  validates_presence_of belong,job_kind
+  validates :job_kind,
+    :numericality => {:only_integer => true, :greater_than => 0}
 end
